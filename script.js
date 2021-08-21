2ed191676cd3c4204371fb046cc56063d32cfe96
LordUttam <47154785+LordUttam@users.noreply.github.com>
2021-08-21 23:48:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let billAmountEntry = document.getElementById("bill");
+let cashReceivedEntry = document.getElementById("cash");
+let next = document.getElementById("next");
+let nextstep = document.getElementById("next-step");
+let enter = document.getElementById("enter");
+let table = document.querySelector("table");
+let message = document.getElementById("message");
+
+function displayNextstep() {
+  let billAmount = parseInt(billAmountEntry.value);
+  if (billAmount < 1) {
+    message.innerHTML = "Please retry with valid bill amount.";
+  } else {
+    nextstep.style.display = "block";
+  }
+}
+
+function getChange() {
+  let billAmount = parseInt(billAmountEntry.value);
+  let cashReceived = parseInt(cashReceivedEntry.value);
+
+  if (billAmount > cashReceived) {
+    message.innerHTML =
+      "Cash is less than bill. Please enter the correct amounts.";
+  } else if (billAmount === cashReceived) {
+    message.innerHTML = "No amount needs to be returned.";
+  } else {
+    table.style.display = "table";
+    let amountleft = cashReceived - billAmount;
+    let notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+
+    for (note of notes) {
+      document.getElementById(note).innerHTML = "";
+    }
+
+    for (note of notes) {
+      if (amountleft >= note) {
+        amountleft = getNotes(amountleft, note);
+      }
+    }
+  }
+}
+
+function getNotes(amountleft, note) {
+  document.getElementById(note).innerHTML = Math.floor(amountleft / note);
+  console.log("hi");
+  return amountleft % note;
+}
+
+enter.addEventListener("click", getChange);
+next.addEventListener("click", displayNextstep);
