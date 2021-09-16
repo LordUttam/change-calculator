32614fc225642d394d1db1838bce9f66a18ad060
LordUttam <47154785+LordUttam@users.noreply.github.com>
2021-09-16 22:43:59 +0530

    Fix: input constraints

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ let table = document.querySelector("table");
 let message = document.getElementById("message");
 
 function displayNextstep() {
+  message.innerText = "";
   let billAmount = parseInt(billAmountEntry.value);
   if (billAmount < 1) {
-    message.innerHTML = "Please retry with valid bill amount.";
+    message.innerText = "Please retry with valid bill amount.";
   } else {
     nextstep.style.display = "block";
   }
@@ -20,11 +21,12 @@ function getChange() {
   let cashReceived = parseInt(cashReceivedEntry.value);
 
   if (billAmount > cashReceived) {
-    message.innerHTML =
+    message.innerText =
       "Cash is less than bill. Please enter the correct amounts.";
   } else if (billAmount === cashReceived) {
-    message.innerHTML = "No amount needs to be returned.";
+    message.innerText = "No amount needs to be returned.";
   } else {
+    message.innerText = "";
     table.style.display = "table";
     let amountleft = cashReceived - billAmount;
     let notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
@@ -47,5 +49,31 @@ function getNotes(amountleft, note) {
   return amountleft % note;
 }
 
-enter.addEventListener("click", getChange);
-next.addEventListener("click", displayNextstep);
+function enterClickHandler() {
+  if (billAmountEntry.value != "" && cashReceivedEntry.value != "") {
+    if (billAmountEntry.value > 0 && cashReceivedEntry.value > 0) {
+      getChange();
+    } else {
+      message.innerText =
+        "Bill amount and cash received can not be zero or negative. Try again.";
+    }
+  } else {
+    message.innerText =
+      "Bill amount and cash received can not be blank. Try again.";
+  }
+}
+
+function nextClickHandler() {
+  if (billAmountEntry.value != "") {
+    if (billAmountEntry.value > 0) {
+      displayNextstep();
+    } else {
+      message.innerText = "Bill amount can not be zero or negative. Try again.";
+    }
+  } else {
+    message.innerText = "Bill amount can not be blank. Try again.";
+  }
+}
+
+enter.addEventListener("click", enterClickHandler);
+next.addEventListener("click", nextClickHandler);
